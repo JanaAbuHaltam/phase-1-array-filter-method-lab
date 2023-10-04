fee4f0bee4a6d18182df0b3eda1b96a860daebf3
Jana Salah <janaabuhaltam@gmail.com>
2023-10-04 08:24:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => {
+            if (driver.toLowerCase() === name.toLowerCase() ) {
+                return driver;
+            }
+        }
+        );
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => { 
+        if (driver.indexOf(letters) ===0 ) {
+        return driver;
+    }
+}
+    )
+}
+
+function matchName(driversObject, name) {
+    return driversObject.filter(driver => driver.name === name)
+}
+ 
+  
